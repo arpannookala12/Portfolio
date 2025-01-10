ab72329c3f5d7552df13eac1bea97fef2fd943eb
arpannookala12 <arpan.nookala@rutgers.edu>
2025-01-10 17:26:48 -0500

    Commit initial version for verification

---
+++ assets/images/js/main.js
@@ -0,0 +1,8 @@
+// Example: Dark Mode Toggle
+const toggleDarkMode = () => {
+    document.body.classList.toggle('dark-mode');
+  };
+  
+  // Attach to a button
+  document.getElementById('darkModeBtn').addEventListener('click', toggleDarkMode);
+  
